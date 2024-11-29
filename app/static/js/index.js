58023b957c83d2457dcb87ac9d46040a5e281389
Eric Klaus <ericklaus16@gmail.com>
2024-11-29 14:56:49 -0300

    enhance investment form with new input fields and JavaScript functionality; update CSS for layout adjustments

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,5 +1,26 @@
+checkedStocks = false;
+checkedFunds = false;
+
 document.querySelector("#investment-type-stocks").addEventListener("click", function() {
-    if(document.querySelector("#investment-type-stocks").checked && document.querySelector("#investment-type-stocks")) {
-        document.querySelector(".investment-interest-area").style.display = "block";
-    } 
+    checkedStocks = !checkedStocks;
+    toggleInvestmentInterest()
 })
+
+document.querySelector("#investment-type-funds").addEventListener("click", function() {
+    checkedFunds = !checkedFunds;
+    toggleInvestmentInterest()
+})
+
+function toggleInvestmentInterest() {
+    document.querySelector('.investment-interest-area').style.display = (checkedStocks || checkedFunds) ? 'block' : 'none';
+}
+
+
+
+function handleCalculateInvestment() {
+    let investmentAmount = document.querySelector("#investment-amount").value;
+    let investmentType = document.querySelector("#investment-type-stocks:checked").value;
+    let investmentInterest = document.querySelector("#investments").value.split(",").map(item => item.trim()).filter(item => item);
+    let investmentRisk = document.querySelector('input[name="risk"]:checked').value;
+    alert(investmentType + " " + investmentAmount + " " + investmentInterest + " " + investmentRisk);
+}
