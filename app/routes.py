f210a218ff351a825d66d877036da88b6773ce2f
Eric Klaus <ericklaus16@gmail.com>
2024-12-06 11:43:04 -0300

    add risk calculation endpoint and implement investment risk assessment logic; update frontend to handle new calculations

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, render_template, url_for
-import os
+from flask import Blueprint, render_template, request, jsonify
+import yfinance as yf
+
 from .utils import create_forecast_plot, create_distribution_plot, create_risk_plot
+from .calculations import calcular_risco_ativos
 
 main = Blueprint('main', __name__)
 
@@ -16,3 +18,30 @@ def about():
     risk_plot_path = create_risk_plot()
 
     return render_template('results.html', forecast_plot_url=forecast_plot_path, distribution_plot_url=distribution_plot_path, risk_plot_url=risk_plot_path)
+
+@main.route('/calcular-risco', methods=['POST'])
+def calcular_risco():
+    data = request.get_json()  # Recebe os dados enviados no fetch
+    investment_amount = data.get("investmentAmount")
+    investment_type = data.get("investmentType")
+    investment_interest = data.get("investmentInterest")  # Lista de ativos
+    investment_risk = data.get("investmentRisk")  # Risco selecionado pelo usuário
+
+    resultados = []
+
+    for ativo in investment_interest:
+        try:
+            ativo_br = ativo + ".SA"
+            calcular_risco_ativos(ativo_br)
+        except Exception as e:
+            resultados.append({
+                "ativo": ativo,
+                "erro": f"Erro ao calcular: {str(e)}"
+            })  
+
+    return jsonify({
+        "investment_type": investment_type,
+        "investment_amount": investment_amount,
+        "investment_risk": investment_risk,
+        "resultados": resultados
+    })
--- app/static/js/index.js
+++ app/static/js/index.js
@@ -15,12 +15,26 @@ function toggleInvestmentInterest() {
     document.querySelector('.investment-interest-area').style.display = (checkedStocks || checkedFunds) ? 'block' : 'none';
 }
 
-
-
 function handleCalculateInvestment() {
     let investmentAmount = document.querySelector("#investment-amount").value;
     let investmentType = document.querySelector("#investment-type-stocks:checked").value;
     let investmentInterest = document.querySelector("#investments").value.split(",").map(item => item.trim()).filter(item => item);
     let investmentRisk = document.querySelector('input[name="risk"]:checked').value;
     alert(investmentType + " " + investmentAmount + " " + investmentInterest + " " + investmentRisk);
+
+    fetch('/calcular-risco', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            investmentAmount,
+            investmentType,
+            investmentInterest,
+            investmentRisk
+        })
+    }).then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
 }
